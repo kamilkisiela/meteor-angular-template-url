db216b38a8f363ac6c725233cf200f42c1ef14f8
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-11-27 13:05:29 +0100

    chore: Add package git and summary

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'mys:angular-template-url',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Makes path of ng template compatible with all Meteor releases',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://githubc.com/kamilkisiela/version-compare.git',
+  git: 'https://github.com/kamilkisiela/meteor-angular-template-url.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
